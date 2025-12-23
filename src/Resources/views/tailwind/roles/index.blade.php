b1df6f4937ff0d06868c3e71f5aee0bfdefda14a
Atik Ullah Nasar <atik1@sahajjo.com>
2025-12-23 18:37:54 +0600

    Update index.blade.php

--- src/Resources/views/tailwind/roles/index.blade.php
+++ src/Resources/views/tailwind/roles/index.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Tailwind CSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
