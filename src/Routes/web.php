d81120b9652a010dfa12093e9cc102a4cf0d8855
Atik Ullah Nasar <atik1@sahajjo.com>
2025-12-25 09:57:47 +0600

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,7 +4,7 @@ use atikullahnasar\role\Http\Controllers\RoleController;
 use atikullahnasar\role\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web'])->prefix('beft')->group(function () {
+Route::middleware(['web', 'auth'])->prefix('beft')->group(function () {
     Route::resource('roles', RoleController::class);
     Route::resource('permissions', PermissionController::class);
 });
